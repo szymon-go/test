b8e3f147b31aaec33e159c5b6a6d8a94f028d30f
Szymon Pasko <szymon.pasko@auto1.com>
2016-06-09 00:02:14 +0200

    add conf mode

--- cmd.sh
+++ cmd.sh
@@ -7,6 +7,8 @@ if [ "$ENV" = 'Development' ]; then
     cd /srv/app/current && php -S 0.0.0.0:8000 index.php
 elif [ "$ENV" = 'UNIT' ]; then
     php ./vendor/bin/phpspec run
+elif [ "$ENV" = 'CONF' ]; then
+    echo "Running CONF Server" 
 else
     echo "Running Production Server"
     cd /srv/app/current && php -S 0.0.0.0:8000 index.php
