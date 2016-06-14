2b935c6a00107f7e5c9e2acb0d2af422b0852ebf
Szymon Pasko <szymon.pasko@auto1.com>
2016-06-14 09:29:14 +0200

    fix project path

--- cmd.sh
+++ cmd.sh
@@ -9,7 +9,7 @@ elif [ "$ENV" = 'UNIT' ]; then
     php /srv/app/current/vendor/bin/phpspec run
 elif [ "$ENV" = 'CONF' ]; then
     echo "Running CONF Server"
-    cd /srv/app/current && composer install
+    cd /srv/app/current/app && composer install
 else
     echo "Running Production Server"
     cd /srv/app/current && php -S 0.0.0.0:8000 index.php
