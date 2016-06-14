34ecaf130038300298101d3777f6dcbe90930006
Szymon Pasko <szymon.pasko@auto1.com>
2016-06-14 09:55:57 +0200

    add install composer

--- cmd.sh
+++ cmd.sh
@@ -6,6 +6,7 @@ if [ "$ENV" = 'Development' ]; then
     echo "Running Development Server" 
     cd /srv/app/current && php -S 0.0.0.0:8000 index.php
 elif [ "$ENV" = 'UNIT' ]; then
+    cd /srv/app/current && composer install
     php /srv/app/current/vendor/bin/phpspec run
 elif [ "$ENV" = 'CONF' ]; then
     echo "Running CONF Server"
